e24722f3867e72c0807214dd24bd86266ab38886
Peter Danenberg <pcd@roxygen.org>
2010-11-02 16:56:52 -0700

    some TODOs; some working initialization; preliminary error abstractions

--- test-freetds.scm
+++ test-freetds.scm
@@ -1,11 +1,17 @@
 #;(use foreign)
-(use lolevel)
+(use format foreigners lolevel)
 
 (foreign-declare "#include <ctpublic.h>")
 
 (define-foreign-type cs-context* (c-pointer "CS_CONTEXT"))
 (define-foreign-type cs-int integer32)
 (define-foreign-variable cs-version-100 cs-int "CS_VERSION_100")
+;; (define-foreign-variable cs-succeed cs-retcode "CS_SUCCEED")
+;; (define-foreign-variable cs-fail cs-retcode "CS_FAIL")
+(define-foreign-type cs-retcode (enum "CS_RETCODE"))
+(define-foreign-variable cs-fail cs-retcode "CS_FAIL")
+(define-foreign-variable cs-succeed cs-retcode "CS_SUCCEED")
+(define-foreign-variable cs-force-exit cs-retcode "CS_FORCE_EXIT")
 ;;; CS_RETCODE cs_ctx_alloc(CS_INT version, CS_CONTEXT ** ctx);
 ;; (define-external context cs-context*)
 ;; (define-external context (c-pointer "CS_CONTEXT"))
@@ -15,11 +21,38 @@
 ;; (foreign-declare "context = (CS_CONTEXT *) NULL;")
 ;; (define-foreign-variable context (c-pointer "CS_CONTEXT") "NULL")
 ;; (define-foreign-variable context cs-context* "NULL")
-(define cs-ctx-alloc (foreign-lambda cs-int cs_ctx_alloc cs-int (c-pointer cs-context*)))
+(define cs-ctx-alloc
+  (foreign-lambda cs-retcode cs_ctx_alloc cs-int (c-pointer cs-context*)))
+(define ct-init
+  (foreign-lambda cs-retcode ct_init cs-context* cs-int))
+(define ct-exit
+  (foreign-lambda cs-retcode ct_exit cs-context* cs-int))
+(define cs-ctx-drop
+  (foreign-lambda cs-retcode cs_ctx_drop cs-context*))
 ;; (foreign-declare "ret = cs_ctx_alloc(CS_VERSION_100, &context);")
 ;; (cs-ctx-alloc cs-version-100 context)
 ;; (set! context (null-pointer))
+(define (freetds-error location message . arguments)
+  (signal (make-composite-condition
+           (make-property-condition 'exn
+                                    'location location
+                                    'message message
+                                    'arguments arguments)
+           (make-property-condition 'freetds))))
+
+(define (error-on-failure thunk location message . arguments)
+  (if (not (= (thunk) cs-succeed))
+      (apply freetds-error location message arguments)))
+
 (let-location ((context cs-context*))
-              (cs-ctx-alloc cs-version-100 (location context)))
-;; (print context)
-(display 123)
+  (error-on-failure
+   (lambda ()
+     (cs-ctx-alloc cs-version-100 (location context)))
+   'context-allocation
+   "failed to allocate context")
+
+  (error-on-failure
+   (lambda ()
+     (ct-init context cs-version-100))
+   'context-initialization
+   "failed to initialize context"))
