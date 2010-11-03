05f779079bcf473a256f55115602db771d043aa7
Peter Danenberg <pcd@roxygen.org>
2010-11-02 23:27:55 -0800

    cleanup

--- test-freetds.scm
+++ test-freetds.scm
@@ -7,13 +7,11 @@
 (define-foreign-type cs-connection* (c-pointer "CS_CONNECTION"))
 (define-foreign-type cs-client-message* (c-pointer "CS_CLIENTMSG"))
 (define-foreign-type cs-server-message* (c-pointer "CS_SERVERMSG"))
-(define-foreign-type cs-int integer32)
-(define-foreign-variable cs-version-100 cs-int "CS_VERSION_100")
-;; (define-foreign-variable cs-succeed cs-retcode "CS_SUCCEED")
-;; (define-foreign-variable cs-fail cs-retcode "CS_FAIL")
-;; (define-foreign-type cs-retcode (enum "CS_RETCODE"))
-(define-foreign-type cs-retcode integer32)
 (define-foreign-type cs-void* (c-pointer "CS_VOID"))
+(define-foreign-type cs-int integer32)
+(define-foreign-type cs-retcode integer32)
+
+(define-foreign-variable cs-version-100 cs-int "CS_VERSION_100")
 (define-foreign-variable cs-fail cs-retcode "CS_FAIL")
 (define-foreign-variable cs-succeed cs-retcode "CS_SUCCEED")
 (define-foreign-variable cs-force-exit int "CS_FORCE_EXIT")
@@ -22,19 +20,13 @@
 (define-foreign-variable cs-client-message-callback int "CS_CLIENTMSG_CB")
 (define-foreign-variable cs-server-message-callback int "CS_SERVERMSG_CB")
 (define-foreign-variable cs-unused int "CS_UNUSED")
-;;; CS_RETCODE cs_ctx_alloc(CS_INT version, CS_CONTEXT ** ctx);
-;; (define-external context cs-context*)
-;; (define-external context (c-pointer "CS_CONTEXT"))
-;; (foreign-declare "CS_CONTEXT *context;")
-;; (foreign-declare "CS_RETCODE ret;")
-;; (foreign-declare "context = (CS_CONTEXT *) NULL;")
-;; (foreign-declare "context = (CS_CONTEXT *) NULL;")
-;; (define-foreign-variable context (c-pointer "CS_CONTEXT") "NULL")
-;; (define-foreign-variable context cs-context* "NULL")
+
 (define cs-ctx-alloc
   (foreign-lambda cs-retcode cs_ctx_alloc cs-int (c-pointer cs-context*)))
+
 (define ct-init
   (foreign-lambda cs-retcode ct_init cs-context* cs-int))
+
 (define cs-config
   (foreign-lambda cs-retcode
                   cs_config
@@ -44,6 +36,7 @@
                   cs-void*
                   cs-int
                   (c-pointer cs-int)))
+
 (define ct-callback
   (foreign-lambda cs-retcode
                   ct_callback
@@ -52,13 +45,12 @@
                   cs-int
                   cs-int
                   cs-void*))
+
 (define ct-exit
   (foreign-lambda cs-retcode ct_exit cs-context* cs-int))
+
 (define cs-ctx-drop
   (foreign-lambda cs-retcode cs_ctx_drop cs-context*))
-;; (foreign-declare "ret = cs_ctx_alloc(CS_VERSION_100, &context);")
-;; (cs-ctx-alloc cs-version-100 context)
-;; (set! context (null-pointer))
 (define (freetds-error location message retcode . arguments)
   (signal (make-composite-condition
            (make-property-condition 'exn
