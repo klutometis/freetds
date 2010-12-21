0e275c7dff214902e4b829de001c32e558f2c1a0
Peter Danenberg <pcd@roxygen.org>
2010-12-20 17:48:58 -0800

    cruft out

--- test-freetds-module.scm
+++ test-freetds-module.scm
@@ -1,9 +1,5 @@
 #!/usr/bin/env chicken-scheme
 (use freetds debug)
-#;(debug (make-CS_INT*)
-       CS_INT-size
-       (make-CS_BINARY*)
-       CS_BINARY-size)
 (include "test-freetds-secret.scm")
 (call-with-context
  (lambda (context)
