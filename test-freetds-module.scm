90fad212911e520f956f2939982c94e6f68589e6
Peter Danenberg <pcd@roxygen.org>
2011-03-22 16:27:35 -0700

    finally got a fucking segfault!

--- test-freetds-module.scm
+++ test-freetds-module.scm
@@ -12,14 +12,15 @@
     (lambda (connection)
       (call-with-result-set
        connection
-       "CREATE TABLE #harro (a INT, b INT, c INT, d INT, e INT, f INT)"
+       "CREATE TABLE #harro (a VARCHAR(30), b VARCHAR(30), c VARCHAR(30), d VARCHAR(30), e VARCHAR(30), f VARCHAR(30))"
        (lambda (command) (debug (result-values context connection command))))
       (for-each
        (lambda (i)
-         (call-with-result-set
-          connection
-          (format "INSERT INTO #harro VALUES(~a, ~a, ~a, ~a, ~a, ~a)" i i i i i i)
-          (lambda (command) (result-values context connection command))))
+         (let ((i (number->string i)))
+           (call-with-result-set
+            connection
+            (format "INSERT INTO #harro VALUES(~a, ~a, ~a, ~a, ~a, ~a)" i i i i i i)
+            (lambda (command) (result-values context connection command)))))
        (iota (expt 2 10)))
       (call-with-result-set
        connection
